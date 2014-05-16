d607281d3ede897af73d35a3488cbd9efeaa5bcb
fr33jc <fr33jc@gmail.com>
2014-05-15 18:36:08 -0700

    Move RESTOAuthClient to its own module

--- rightscale/rightscale.py
+++ rightscale/rightscale.py
@@ -3,64 +3,10 @@ Python-Rightscale
 
 A stupid wrapper around rightscale's HTTP API
 """
-from functools import partial
-import requests
-
+from .httpclient import RESTOAuthClient
 from .util import get_rc_creds
 
 
-class RESTOAuthClient(object):
-    """
-    HTTP client that is aware of REST and OAuth semantics.
-    """
-    def __init__(self):
-        self.endpoint = ''
-        self.headers = {'Accept': 'application/json'}
-
-        # convenience methods
-        self.get = partial(self.request, 'get')
-        self.post = partial(self.request, 'post')
-
-    def request(self, method, path='/', url=None, ignore_codes=[], **kwargs):
-        """
-        Performs HTTP request.
-
-        :param str method: An HTTP method (e.g. 'get', 'post', 'PUT', etc...)
-
-        :param str path: A path component of the target URL.  This will be
-            appended to the value of ``self.endpoint``.  If both :attr:`path`
-            and :attr:`url` are specified, the value in :attr:`url` is used and
-            the :attr:`path` is ignored.
-
-        :param str url: The target URL (e.g.  ``http://server.tld/somepath/``).
-            If both :attr:`path` and :attr:`url` are specified, the value in
-            :attr:`url` is used and the :attr:`path` is ignored.
-
-        :param list of int ignore_codes: List of HTTP error codes (e.g.
-            404, 500) that should be ignored.  If an HTTP error occurs and it
-            is *not* in :attr:`ignore_codes`, then an exception is raised.
-
-        :param kwargs: Any other kwargs to pass to :meth:`requests.request()`.
-
-        Returns a :class:`requests.Response` object.
-        """
-        _url = url if url else (self.endpoint + path)
-
-        # merge with defaults in headers attribute.  incoming 'headers' take
-        # priority over values in self.headers to allow last-minute overrides
-        # if the caller really knows what they're doing.
-        if 'headers' in kwargs:
-            headers = kwargs.pop('headers')
-            for k, v in self.headers.items():
-                headers.setdefaults(k, v)
-        else:
-            headers = self.headers
-        kwargs['headers'] = headers
-
-        r = requests.request(method, _url, **kwargs)
-        if not r.ok and r.status_code not in ignore_codes:
-            r.raise_for_status()
-        return r
 
 
 class RightScale(object):
