4aa4c196411e57066f63f7c9ad88c550955aa8e3
brantai@gmail.com <brantai@gmail.com>
2014-05-19 11:29:48 -0700

    Ripped out account number and modified util.py to allow specifying a custom api endpoint.

--- rightscale/util.py
+++ rightscale/util.py
@@ -3,13 +3,13 @@ import ConfigParser
 
 CFG_USER_RC = '.rightscalerc'
 CFG_SECTION_OAUTH = 'OAuth'
-CFG_OPTION_ACCT = 'account_id'
+CFG_OPTION_ENDPOINT = 'api_endpoint'
 CFG_OPTION_REF_TOKEN = 'refresh_token'
 
 
 def get_rc_creds():
     """
-    Reads ~/.rightscalerc and returns account ID and refresh token.
+    Reads ~/.rightscalerc and returns API endpoint and refresh token.
 
     Always returns a tuple of strings even if the file is empty - in which
     case, returns ``('', '')``.
@@ -19,7 +19,7 @@ def get_rc_creds():
     # set up some defaults - too bad only newer pythons know how to do this
     # more gracefully:
     config.add_section(CFG_SECTION_OAUTH)
-    config.set(CFG_SECTION_OAUTH, CFG_OPTION_ACCT, '')
+    config.set(CFG_SECTION_OAUTH, CFG_OPTION_ENDPOINT, '')
     config.set(CFG_SECTION_OAUTH, CFG_OPTION_REF_TOKEN, '')
 
     home = os.path.expanduser('~')
@@ -27,7 +27,7 @@ def get_rc_creds():
     try:
         config.read(rc_file)
         return (
-                config.get(CFG_SECTION_OAUTH, CFG_OPTION_ACCT),
+                config.get(CFG_SECTION_OAUTH, CFG_OPTION_ENDPOINT),
                 config.get(CFG_SECTION_OAUTH, CFG_OPTION_REF_TOKEN),
                 )
     except:
