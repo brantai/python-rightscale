e278e494988e8eea9c3583c3702b2d3438de8630
fr33jc <fr33jc@gmail.com>
2014-05-22 13:16:02 -0700

    Cache the config parsed from ~/.rightscalerc.
    This allows multiple consumers of config within a process without having
    to parse the rc file each time.

--- rightscale/util.py
+++ rightscale/util.py
@@ -6,6 +6,25 @@ CFG_SECTION_OAUTH = 'OAuth'
 CFG_OPTION_ENDPOINT = 'api_endpoint'
 CFG_OPTION_REF_TOKEN = 'refresh_token'
 
+_config = None
+
+
+def get_config():
+    global _config
+    if not _config:
+        _config = ConfigParser.SafeConfigParser()
+
+        # set up some defaults - too bad only newer pythons know how to do this
+        # more gracefully:
+        _config.add_section(CFG_SECTION_OAUTH)
+        _config.set(CFG_SECTION_OAUTH, CFG_OPTION_ENDPOINT, '')
+        _config.set(CFG_SECTION_OAUTH, CFG_OPTION_REF_TOKEN, '')
+
+        home = os.path.expanduser('~')
+        rc_file = os.path.join(home, CFG_USER_RC)
+        _config.read(rc_file)
+    return _config
+
 
 def get_rc_creds():
     """
@@ -14,18 +33,8 @@ def get_rc_creds():
     Always returns a tuple of strings even if the file is empty - in which
     case, returns ``('', '')``.
     """
-    config = ConfigParser.SafeConfigParser()
-
-    # set up some defaults - too bad only newer pythons know how to do this
-    # more gracefully:
-    config.add_section(CFG_SECTION_OAUTH)
-    config.set(CFG_SECTION_OAUTH, CFG_OPTION_ENDPOINT, '')
-    config.set(CFG_SECTION_OAUTH, CFG_OPTION_REF_TOKEN, '')
-
-    home = os.path.expanduser('~')
-    rc_file = os.path.join(home, CFG_USER_RC)
+    config = get_config()
     try:
-        config.read(rc_file)
         return (
                 config.get(CFG_SECTION_OAUTH, CFG_OPTION_ENDPOINT),
                 config.get(CFG_SECTION_OAUTH, CFG_OPTION_REF_TOKEN),
