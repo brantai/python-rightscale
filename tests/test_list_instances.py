f6ba3d0e7d2fdc3013883928674c9bdeab962443
fr33jc <fr33jc@gmail.com>
2014-06-05 11:06:13 -0700

    Add more tests to improve coverage

--- tests/test_list_instances.py
+++ tests/test_list_instances.py
@@ -7,3 +7,9 @@ from rightscale import list_instances
 def test_list_all_instances_tiny():
     all_instances = list_instances()
     print "Got %d instances" % len(all_instances)
+
+
+@attr('rc_creds', 'real_conn')
+def test_list_some_instances_tiny():
+    some_instances = list_instances(deployment_name='production')
+    print "Got %d instances" % len(some_instances)
---
+++ tests/test_rc.py
@@ -0,0 +1,39 @@
+import mock
+from rightscale import util
+
+
+def test_read_rc():
+    """
+    get_rc_creds() should return (endpoint, token).
+    """
+    exp_endpoint = 'the endpoint yo'
+    exp_token = 'the ref token'
+
+    def fake_config_parser_get(_, key):
+        return {
+                util.CFG_OPTION_ENDPOINT: exp_endpoint,
+                util.CFG_OPTION_REF_TOKEN: exp_token,
+                }.get(key)
+
+    _get_config = mock.MagicMock()
+    _get_config.return_value.get.side_effect = fake_config_parser_get
+
+    util.get_config = _get_config
+
+    assert (exp_endpoint, exp_token) == util.get_rc_creds()
+
+
+def test_failed_rc():
+    """
+    get_rc_creds() should return empty strings on errors.
+    """
+    _get_config = mock.MagicMock()
+    _get_config.return_value.get.side_effect = ['first', ValueError]
+    util.get_config = _get_config
+    assert ('', '') == util.get_rc_creds()
+
+    _get_config.return_value.get.side_effect = [NameError, 'last']
+    assert ('', '') == util.get_rc_creds()
+
+    _get_config.return_value = ValueError
+    assert ('', '') == util.get_rc_creds()
