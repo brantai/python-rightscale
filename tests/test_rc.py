f876a282a9343488ffa50219f15a1c2fe6e56b53
fr33jc <fr33jc@gmail.com>
2014-06-05 17:49:11 -0700

    Switch to patching in test_rc.py.
    Before this, the mocks in test_rc.py were not being removed.  This
    caused some cross-test pollution.  Now test_rc.py uses a context manager
    for all the mocking/patching because you get unpatching for free (i.e.
    the original objects are restored after each test).

--- tests/test_rc.py
+++ tests/test_rc.py
@@ -1,4 +1,4 @@
-import mock
+from mock import patch
 from rightscale import util
 
 
@@ -15,25 +15,21 @@ def test_read_rc():
                 util.CFG_OPTION_REF_TOKEN: exp_token,
                 }.get(key)
 
-    _get_config = mock.MagicMock()
-    _get_config.return_value.get.side_effect = fake_config_parser_get
-
-    util.get_config = _get_config
-
-    assert (exp_endpoint, exp_token) == util.get_rc_creds()
+    with patch('rightscale.util.get_config') as _get_config:
+        _get_config.return_value.get.side_effect = fake_config_parser_get
+        assert (exp_endpoint, exp_token) == util.get_rc_creds()
 
 
 def test_failed_rc():
     """
     get_rc_creds() should return empty strings on errors.
     """
-    _get_config = mock.MagicMock()
-    _get_config.return_value.get.side_effect = ['first', ValueError]
-    util.get_config = _get_config
-    assert ('', '') == util.get_rc_creds()
+    with patch('rightscale.util.get_config') as _get_config:
+        _get_config.return_value.get.side_effect = ['first', ValueError]
+        assert ('', '') == util.get_rc_creds()
 
-    _get_config.return_value.get.side_effect = [NameError, 'last']
-    assert ('', '') == util.get_rc_creds()
+        _get_config.return_value.get.side_effect = [NameError, 'last']
+        assert ('', '') == util.get_rc_creds()
 
-    _get_config.return_value = ValueError
-    assert ('', '') == util.get_rc_creds()
+        _get_config.return_value = ValueError
+        assert ('', '') == util.get_rc_creds()
