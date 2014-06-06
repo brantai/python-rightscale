c1709b73a333288d0fcaaeb67bd8c2b01293cfd4
fr33jc <fr33jc@gmail.com>
2014-06-05 17:51:59 -0700

    Document how to use test_run_script.py

--- tests/test_run_script.py
+++ tests/test_run_script.py
@@ -1,3 +1,27 @@
+"""
+Tests for successful execution of RightScript.
+
+Note that this is just from the viewpoint of the RightScale API.  You still
+have to provide a RightScript that works.
+
+To make these tests run, add a section like the following to your
+~/.rightscalerc::
+
+    [Testing]
+    safe_script_name = Name of some RightScript
+    safe_target_server_name = Ops Test Target
+    inputs = Key1:value one,key 2:value2
+
+Where ``inputs`` is a comma-separated list of key-value pairs, each of which is
+the key string, a colon, and the value string.  The ``inputs`` example above
+would be parsed to the following Python object::
+
+    {
+        'Key1': 'value one',
+        'key 2': 'value2',
+    }
+
+"""
 from StringIO import StringIO
 from nose.plugins.attrib import attr
 
@@ -58,7 +82,6 @@ if _SCRIPT and _TARGET:
                 inputs=_INPUTS,
                 timeout_s=25,
                 )
-
 
     @attr('rc_creds', 'real_conn')
     def test_run_script_timeout():
