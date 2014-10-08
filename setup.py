c56372dea7f3f28a1ddaa572028cfa78a84e7b77
Matt Wise <matt@nextdoor.com>
2014-10-08 09:10:24 -0700

    Fix version-reading in setup.py that prevented pip install.
    It was impossible to use pip to install the code because the setup.py
    file imported rightscale/__init__.py, which subsequently imported the
    entire application and all of its dependencies, causing a circular loop.
    
    Since the VERSION tag isn't used anywhere but the setup.py, I've moved
    it into its own text file and updated setup.py to read the version from
    there. This cleans up the installation via pip and allows for
    python-rightscale to be installed via a requirements.txt file easily.

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@ try:
 except:
     from distutils.core import setup
 
-from rightscale import VERSION
-
 packages = ['rightscale']
-requires = ['requests']
 
 
 setup(name='python-rightscale',
-      version=VERSION,
+      version=open('version.txt').read().strip(),
       description='Python wrapper for the Rightscale API',
       author='Brent Naylor',
       author_email='brantai@gmail.com',
@@ -18,5 +15,6 @@ setup(name='python-rightscale',
       package_dir={'rightscale': 'rightscale'},
       url='https://github.com/brantai/python-rightscale',
       packages=packages,
-      install_requires=requires,
+      setup_requires=open('requirements.txt').readlines(),
+      install_requires=open('requirements.txt').readlines()
       )
